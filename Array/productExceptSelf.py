bc4447e89591b0f55d9d73e1a8209d33ced61587
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-01-31 00:12:40 -0500

    Update productExceptSelf.py

--- Array/productExceptSelf.py
+++ Array/productExceptSelf.py
@@ -1,15 +1,39 @@
 class Solution:
-    def productExceptSelf(self, nums):
-        res = [1] * (len(nums))
-        prefix = 1
+    # def productExceptSelf(self, nums):
+    #     res = [1] * (len(nums))
+    #     prefix = 1
+    #     for i in range(len(nums)):
+    #         res[i] = prefix
+    #         prefix *= nums[i]
+    #     postfix = 1
+    #     for j in range(len(nums)-1, -1, -1):
+    #         res[j] *= postfix
+    #         postfix *= nums[j]
+    #     return res
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        # create two arrays - postfix and prefix
+
+        postfix = [1] * len(nums)
+        prefix = [1] * len(nums)
+
+        # Prefix and postfix contains values including the self
+        # Remove the self
+
+        for i in range(1, len(nums)):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+
+        for i in range(len(nums) - 2, -1, -1):
+            postfix[i] = postfix[i + 1] * nums[i + 1]
+        
+        print(prefix, postfix)
+
+        res = []
         for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for j in range(len(nums)-1, -1, -1):
-            res[j] *= postfix
-            postfix *= nums[j]
-        return res
+            res.append(prefix[i] * postfix[i])
+
+        return res         
 
 
 out = Solution().productExceptSelf([1,2,3,4])
