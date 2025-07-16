c1a18d4d848f6fb480a7f44ac4162b3227956001
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-07-15 23:22:57 -0400

    Update longest_common_subsequence.py

--- Dynamic Programming/longest_common_subsequence.py
+++ Dynamic Programming/longest_common_subsequence.py
@@ -1,19 +1,13 @@
 class Solution:
-    def longestPalindromeSubseq(self, s: str) -> int:
-        n = len(s)
-        text2=s[::-1]
-        
-        dp = [[0] * (n + 1) for _ in range(n + 1)] 
-
-        for i in range(n):
-            for j in range(n):
-
-                if s[i]==text2[j]:
-                    dp[i][j]=1+dp[i-1][j-1]
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        grid = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    grid[i][j] = 1 + grid[i + 1][j + 1]
                 else:
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-                    
-        return dp[n-1][n-1]
+                    grid[i][j] = max(grid[i][j + 1], grid[i + 1][j])
+        return grid[0][0]
 
 
 out = Solution().longestCommonSubsequence("abcde", "ace")
