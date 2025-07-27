efb5633e6e4f9d52d65c44a5a0e32348d2b18583
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-07-27 18:52:48 -0400

    Create searchInsert.py

---
+++ Array/searchInsert.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        left = 0
+        right = len(nums) - 1
+        found = False
+
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
+
+sol = Solution(nums = [1,3,5,6], target = 5)
+print(sol)
